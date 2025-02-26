54d4427a225e7c2daaaa48a2ff86271a4be075df
Malik Teague <malikisiah214@gmail.com>
2025-02-26 09:33:45 -0600

    removed gluestack UI

--- utils/queryClient.ts
+++
@@ -1,7 +0,0 @@
-// queryClient.js
-import { QueryClient } from "@tanstack/react-query";
-
-// Create a single QueryClient instance
-const queryClient = new QueryClient();
-
-export default queryClient;
