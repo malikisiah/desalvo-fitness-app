f3c4c317494c45095c86e735f13253c1dbaf347a
Malik Teague <malikisiah214@gmail.com>
2025-02-26 13:09:07 -0600

    init React-Native-Elements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,30 @@
 import { Stack } from "expo-router";
+import queryClient from "@/utils/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, createTheme } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  const theme = createTheme({
+    lightColors: {
+      primary: "#008dcf",
+
+      black: "white",
+      background: "#080808",
+    },
+    mode: "light",
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
+        <StatusBar style="light" />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,79 @@
-import { Text, View } from "react-native";
+import { View, FlatList, TouchableWithoutFeedback } from "react-native";
+import { Text } from "@rneui/themed";
+import { useTheme, Card } from "@rneui/themed";
+import { useQuery } from "@tanstack/react-query";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { supabase } from "@/utils/supabase";
 
 export default function Index() {
+  const { theme } = useTheme();
+
+  const { data } = useQuery({
+    queryKey: ["data"],
+    queryFn: async () => {
+      const { data } = await supabase.from("myTable").select();
+      return data;
+    },
+  });
+
   return (
-    <View
+    <SafeAreaView
       style={{
+        backgroundColor: theme.colors.background,
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text h2> Main Page</Text>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableWithoutFeedback onPress={() => console.log(item.content)}>
+              <Card
+                containerStyle={{
+                  backgroundColor: "black",
+                  borderWidth: 0,
+                  minWidth: 275,
+                }}
+                wrapperStyle={{
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  borderColor: theme.colors.grey0,
+                  padding: 10,
+                }}
+              >
+                <Card.Title>
+                  <Text>{item.id}</Text>
+                </Card.Title>
+                <Card.Divider />
+                <Card.Image
+                  source={{
+                    uri: "https://images.unsplash.com/photo-1616803689943-5601631c7fec?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  }}
+                />
+                <Text style={{ marginBottom: 10, textAlign: "center" }}>
+                  {item.content}
+                </Text>
+              </Card>
+            </TouchableWithoutFeedback>
+          )}
+          contentContainerStyle={{
+            flexGrow: 1,
+            width: "100%",
+            alignItems: "center",
+            paddingBottom: 20,
+          }}
+          style={{ width: "100%" }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
