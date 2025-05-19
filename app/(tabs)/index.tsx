9cc920ba4ee55158820652fa0f9228027afbcb5e
Malik Teague <malikisiah214@gmail.com>
2025-05-19 18:01:50 -0500

    fix: handle and display error messages in notification context

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,13 +5,19 @@ import { useNotification } from "@/context/NotificationContext";
 import SafeAreaView from "@/components/ui/SafeAreaView";
 
 export default function Index() {
-  const { expoPushToken, notification } = useNotification();
+  const { expoPushToken, notification, error } = useNotification();
   return (
     <SafeAreaView>
       <Box justifyContent="center" alignItems="center" flex={1} gap="l">
         <Text variant="header">My Push Token</Text>
         <Box margin="m">
           <Text>{expoPushToken}</Text>
+          {error && (
+            <Box>
+              <Text>{error.message}</Text>
+              <Text>{String(error.cause)}</Text>
+            </Box>
+          )}
         </Box>
         <Text variant="header">Latest Notification</Text>
         <Box margin="m">
