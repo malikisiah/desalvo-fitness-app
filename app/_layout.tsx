8336927c3c86535a42358848386b64d36c148643
Malik Teague <malikisiah214@gmail.com>
2026-03-27 14:04:27 -0500

    rebuilt app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,86 +1,25 @@
-import { Stack, useRouter, useSegments } from "expo-router";
-import queryClient from "@/utils/query-client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "@shopify/restyle";
-import { useFonts } from "@expo-google-fonts/inter";
-import theme from "@/utils/theme";
-import * as SplashScreen from "expo-splash-screen";
-import * as Notifications from "expo-notifications";
+import '@/global.css';
 
-import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
-import { NotificationProvider } from "@/context/NotificationContext";
-import { useAuthStore } from "@/utils/authStore";
+import { NAV_THEME } from '@/lib/theme';
+import { ThemeProvider } from '@react-navigation/native';
+import { PortalHost } from '@rn-primitives/portal';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'nativewind';
 
-SplashScreen.preventAutoHideAsync();
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => {
-    // This sets how the notification should behave when it is received
-    return {
-      shouldShowAlert: true, // Show an alert
-      shouldPlaySound: false, // Don't play a sound
-      shouldSetBadge: false, // Don't set the badge on the app icon
-    };
-  },
-});
-
-function RootLayoutNav() {
-  const segments = useSegments();
-  const router = useRouter();
-  const { session } = useAuthStore();
-
-  useEffect(() => {
-    const inAuthGroup = segments[0] === "(tabs)";
-    const onSignIn = segments[0] === "sign-in";
-
-    if (!session && inAuthGroup) {
-      // Redirect to sign-in if not authenticated and trying to access tabs
-      router.replace("/sign-in");
-    } else if (session && onSignIn) {
-      // Redirect to tabs if authenticated and on sign-in page
-      router.replace("/(tabs)");
-    }
-  }, [session, segments, router]);
-
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: "fade_from_bottom",
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="sign-in" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
-  );
-}
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    Manrope: require("../assets/fonts/Manrope.ttf"),
-    BebasNeue: require("../assets/fonts/BebasNeue.ttf"),
-    Urbanist: require("../assets/fonts/Urbanist.ttf"),
-  });
+  const { colorScheme } = useColorScheme();
 
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
   return (
-    <NotificationProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <RootLayoutNav />
-          <StatusBar style="dark" />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </NotificationProvider>
+    <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <Stack />
+      <PortalHost />
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,77 @@
-import { Redirect } from "expo-router";
-import { useAuthStore } from "@/utils/authStore";
+import { Button } from '@/components/button';
+import { Icon } from '@/components/icon';
+import { Text } from '@/components/text';
+import { Link, Stack } from 'expo-router';
+import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
+import { useColorScheme } from 'nativewind';
+import * as React from 'react';
+import { Image, type ImageStyle, View } from 'react-native';
 
-export default function Index() {
-  const { session } = useAuthStore();
+const LOGO = {
+  light: require('@/assets/images/react-native-reusables-light.png'),
+  dark: require('@/assets/images/react-native-reusables-dark.png'),
+};
 
-  // Simple redirect based on auth state
-  if (session) {
-    return <Redirect href="/(tabs)" />;
-  }
+const SCREEN_OPTIONS = {
+  title: 'React Native Reusables',
+  headerTransparent: true,
+  headerRight: () => <ThemeToggle />,
+};
 
-  return <Redirect href="/sign-in" />;
+const IMAGE_STYLE: ImageStyle = {
+  height: 76,
+  width: 76,
+};
+
+export default function Screen() {
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <>
+      <Stack.Screen options={SCREEN_OPTIONS} />
+      <View className="flex-1 items-center justify-center gap-8 p-4">
+        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
+        <View className="gap-2 p-4">
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            1. Edit <Text variant="code">app/index.tsx</Text> to get started.
+          </Text>
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            2. Save to see your changes instantly.
+          </Text>
+        </View>
+        <View className="flex-row gap-2">
+          <Link href="https://reactnativereusables.com" asChild>
+            <Button>
+              <Text>Browse the Docs</Text>
+            </Button>
+          </Link>
+          <Link href="https://github.com/founded-labs/react-native-reusables" asChild>
+            <Button variant="ghost">
+              <Text>Star the Repo</Text>
+              <Icon as={StarIcon} />
+            </Button>
+          </Link>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const THEME_ICONS = {
+  light: SunIcon,
+  dark: MoonStarIcon,
+};
+
+function ThemeToggle() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <Button
+      onPressIn={toggleColorScheme}
+      size="icon"
+      variant="ghost"
+      className="ios:size-9 rounded-full web:mx-4">
+      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+    </Button>
+  );
 }
